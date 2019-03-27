bd1e0d075671a9e828c52ecb1de194250dc5227a
duyanh.pham <duyanh.pham@inno4b.de>
2019-03-28 00:11:32 +0100

    fix git conflict

--- Node+arduino_seriell/Arduino_side/src/main.cpp
+++ Node+arduino_seriell/Arduino_side/src/main.cpp
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /*
 Duy Anh Pham
 */
@@ -24,16 +23,6 @@ Dev platform: platform.io - VSCode
 SoftwareSerial NodeMcu(2,3);
 //data package size: max. 4 bytes 
 int dataReceived;
-=======
-#include <Arduino.h>
-#include <SoftwareSerial.h>
-
-////////////////
-/////Arduino////
-////////////////
-SoftwareSerial NodeMcu(2,3);
-int dataReceived; //Initialized variable to store recieved data
->>>>>>> 07b1eddcdc173f1222118e8069eccccb071a1b83
 int dataSend;
 void setup() {
   //Serial Begin at 9600 Baud 
@@ -44,7 +33,6 @@ void setup() {
 
 void loop() {
   //Read the serial data and store it
-<<<<<<< HEAD
   Serial.println();
   dataReceived = NodeMcu.read();
   Serial.println("Listening to NodeMcu:");
@@ -57,18 +45,4 @@ void loop() {
   Serial.println(dataSend);
   NodeMcu.write(dataSend);
   delay(5000);
-=======
-  dataReceived = NodeMcu.read();
-  Serial.println("Listening to NodeMcu:");
-  Serial.println(dataReceived);
-  // Serial.println(dataReceived, HEX); //Serial.println(int data, int base); data size = 4bytes
-  // Serial.println(dataReceived, BIN);
-  
-  //Sending back the data
-  dataSend = 0x55;
-  Serial.println("Sending to NodeMcu:");
-  Serial.println(dataSend);
-  NodeMcu.write(dataSend);
-  delay(1000);
->>>>>>> 07b1eddcdc173f1222118e8069eccccb071a1b83
 }
--- Node+arduino_seriell/NodeMcu_side/src/main.cpp
+++ Node+arduino_seriell/NodeMcu_side/src/main.cpp
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /*
 Duy Anh Pham
 */
@@ -21,36 +20,18 @@ Dev platform: platform.io - VSCode
 #include <Arduino.h>
 #include<SoftwareSerial.h> //Included SoftwareSerial Library
 
-=======
-#include <Arduino.h>
-#include<SoftwareSerial.h> //Included SoftwareSerial Library
-
-////////////////
-/////NodeMCU////
-////////////////
-
-//Started SoftwareSerial at RXD2(13) and TXD2(15) pin of ESP8266/NodeMCU
-// SoftwareSerial arduino(3,1);
->>>>>>> 07b1eddcdc173f1222118e8069eccccb071a1b83
 SoftwareSerial arduino(13,15);
 int dataSend;
 int dataRecieve;
 
 void setup() {
-<<<<<<< HEAD
-=======
-  //Serial S Begin at 9600 Baud
->>>>>>> 07b1eddcdc173f1222118e8069eccccb071a1b83
   arduino.begin(9600);
   Serial.begin(9600); 
 }
 
 void loop() {
   //data size by Serial.write() is max. 4bytes
-<<<<<<< HEAD
   Serial.println();
-=======
->>>>>>> 07b1eddcdc173f1222118e8069eccccb071a1b83
   dataSend = 0xFF;
   arduino.write(dataSend);
   Serial.print("Sending to arduino: ");
@@ -58,12 +39,5 @@ void loop() {
   Serial.print("Receiving from arduino:");
   dataRecieve = arduino.read();
   Serial.println(dataRecieve);
-<<<<<<< HEAD
   delay(5000);
-=======
-  delay(1000);
-  // dataRecieve = Serial.read();
-  // Serial.println(dataRecieve);
-  // delay(1000);
->>>>>>> 07b1eddcdc173f1222118e8069eccccb071a1b83
 }
